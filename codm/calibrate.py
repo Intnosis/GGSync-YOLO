f60a205a916737b098495110b3e9c3313aa0e982
Ma. Cristina V. Sison <macristinasison55@gmail.com>
2026-08-26 16:03:35 +0800

    codm

---
+++ codm/calibrate.py
@@ -0,0 +1,28 @@
+import cv2
+
+frame = cv2.imread("frames/frame_0001.png")
+clone = frame.copy()
+
+def click_event(event, x,y,flags, param):
+    if event == cv2.EVENT_LBUTTONDOWN:
+        print(f"clicked_at: ({x}), ({y})")
+        cv2.circle(clone, (x,y), 3, (0,255,0), -1)
+        cv2.imshow("frame", clone)
+
+def player_click_event(event,x,y,flags,param):
+    if event == cv2.EVENT_LBUTTONDOWN:
+        print(f"clicked at: ({x}), ({y})")
+        cv2.circle(clone, (x,y), 3, (0,255,0), -1)
+        cv2.imshow("frame",clone)
+
+cv2.imshow("frame",clone)
+cv2.setMouseCallback("frame", player_click_event)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+
+# MINMAP_ROI = {"x1":10, "y1": 460, "x2":300, "y2":730}
+# PLAYER_BAR_ROI = {"x1": 0, "y1": 0, "x2": 960, "y2":100}
+
+# minimap_crop=frame[460:730, 10:300]
+# player_bar_crop=frame[0:100, 0:960]
